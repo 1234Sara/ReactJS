af8ec8321c291d7245906df4c0a7649dc7824508
1234Sara <sarahabdo38@gmail.com>
2023-07-23 23:35:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React, { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import {
+  Button,
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">
+        <header>
+          <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+              <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: "100px" }}
+                  navbarScroll
+                >
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <NavDropdown title="Link" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#" disabled>
+                    Link
+                  </Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+
+          <br />
+
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>React JS</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                This is card for our React Project
+              </Card.Subtitle>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+
+          <br />
+          <Card style={{ width: "18rem" }}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+          </Card>
+          <br />
+          {[
+            "Primary",
+            "Secondary",
+            "Success",
+            "Danger",
+            "Warning",
+            "Info",
+            "Light",
+            "Dark",
+          ].map((variant) => (
+            <Card
+              bg={variant.toLowerCase()}
+              key={variant}
+              text={variant.toLowerCase() === "light" ? "dark" : "white"}
+              style={{ width: "18rem" }}
+              className="mb-2"
+            >
+              <Card.Header>Header</Card.Header>
+              <Card.Body>
+                <Card.Title>{variant} Card Title </Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
+        </header>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
